4a34c0f81595f243acad19f16ae131054f8655c5
Hackbright Student <user@mini-002.(none)>
2013-03-06 16:43:16 -0800

    This is our final Project X

--- projectx.py
+++ projectx.py
@@ -5,12 +5,12 @@ import shutil
 alphabet = list(string.lowercase)
 
 # makes a bunch of folders, each named for a letter in the alphabet
-#for letter in alphabet:
-#	os.mkdir(letter)
+for letter in alphabet:
+	os.mkdir(letter)
 
 word_list = os.listdir("original_files/files/")
 
-# gets file at position 0 in word_list, grabs first letter of that file, copies file to a directory named that first letter
+# gets file at position 0 in word_list, grabs first letter of that file, copies file to a directory named that first letter. This was test code for proof of concept.
 #print word_list[0]
 #print (word_list[0])[0]
 #shutil.copy("original_files/files/" + word_list[0], (word_list[0])[0])
@@ -18,4 +18,5 @@ word_list = os.listdir("original_files/files/")
 for i in range(len(word_list)):
 	shutil.copy("original_files/files/" + word_list[i], (word_list[i])[0])
 
+# deletes original text files, we're doing copy + delete since shutil.move requires that the directories don't already exist
 shutil.rmtree("original_files/files/")
